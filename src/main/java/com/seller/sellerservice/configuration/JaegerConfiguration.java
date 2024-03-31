35a993fa3609db38e8248715a3e50a524f1b2b0c
Ten Aleksandr Aleksandrovich <89169604848t@gmail.com>
2024-03-31 16:24:36 +0300

    Added jaeger

---
+++ src/main/java/com/seller/sellerservice/configuration/JaegerConfiguration.java
@@ -0,0 +1,20 @@
+package com.seller.sellerservice.configuration;
+
+import io.opentracing.Tracer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JaegerConfiguration {
+    @Bean
+    public Tracer jaegerTracer(){
+        return new io.jaegertracing.Configuration("AuthService")
+                .withSampler(new io.jaegertracing.Configuration.SamplerConfiguration().withType("const").withParam(1))
+                .withReporter(new io.jaegertracing.Configuration.ReporterConfiguration()
+                        .withLogSpans(true)
+                        .withSender(new io.jaegertracing.Configuration.SenderConfiguration()
+                                .withEndpoint("http://jaeger.jaeger:14268/api/traces"))) // HTTP endpoint
+                .getTracer();
+    }
+
+}
