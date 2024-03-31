24626aa76cea8d83597780e79a554a10bacae890
Ten Aleksandr Aleksandrovich <89169604848t@gmail.com>
2024-03-31 16:29:22 +0300

    Added traces for methods

--- src/main/java/com/seller/sellerservice/controller/ProductController.java
+++ src/main/java/com/seller/sellerservice/controller/ProductController.java
@@ -3,6 +3,9 @@ package com.seller.sellerservice.controller;
 import com.seller.sellerservice.dto.ProductDTO;
 import com.seller.sellerservice.entity.Product;
 import com.seller.sellerservice.service.ProductService;
+import io.opentracing.Span;
+import io.opentracing.Tracer;
+import io.opentracing.tag.Tags;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,9 +18,13 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ProductController {
     private final ProductService productService;
-
+    private final Tracer tracer;
     @PostMapping("/{userId}")
     public ResponseEntity<String> addNewProduct(@PathVariable Long userId, @RequestBody ProductDTO productDTO) {
+        Span span = tracer.buildSpan("Adding new product").start();
+        Tags.HTTP_METHOD.set(span, "POST");
+        Tags.HTTP_URL.set(span, "/api/product/"+userId);
+        span.finish();
         try {
             productService.createProduct(userId, productDTO);
             return new ResponseEntity<>("Товар добавлен", HttpStatus.CREATED);
@@ -28,6 +35,10 @@ public class ProductController {
 
     @DeleteMapping("/{userId}/{productId}")
     public ResponseEntity<String> deleteProduct(@PathVariable Long userId, @PathVariable Long productId) {
+        Span span = tracer.buildSpan("Delete product").start();
+        Tags.HTTP_METHOD.set(span, "DELETE");
+        Tags.HTTP_URL.set(span, "/api/product/"+userId+"/"+productId);
+        span.finish();
         try {
             productService.deleteProduct(userId, productId);
             return new ResponseEntity<>("Товар успешно удален", HttpStatus.OK);
@@ -38,6 +49,10 @@ public class ProductController {
 
     @GetMapping("/{userId}")
     public ResponseEntity<List<Product>> getAllProducts(@PathVariable Long userId) {
+        Span span = tracer.buildSpan("Get all products").start();
+        Tags.HTTP_METHOD.set(span, "GET");
+        Tags.HTTP_URL.set(span, "/api/product/"+userId);
+        span.finish();
         List<Product> productList = productService.getAllProducts(userId);
         for (Product product : productList)
             System.out.println(product.toString());
@@ -46,6 +61,10 @@ public class ProductController {
 
     @GetMapping("/{userId}/{productId}")
     public ResponseEntity<?> getProductById(@PathVariable Long userId, @PathVariable Long productId) {
+        Span span = tracer.buildSpan("Get product").start();
+        Tags.HTTP_METHOD.set(span, "GET");
+        Tags.HTTP_URL.set(span, "/api/product/"+userId+"/"+productId);
+        span.finish();
         try {
             Product product = productService.getProductById(userId, productId);
             return new ResponseEntity<>(product, HttpStatus.OK);
@@ -57,6 +76,10 @@ public class ProductController {
     @PutMapping("/{userId}/{productId}")
     public ResponseEntity<?> updateProduct(@PathVariable Long userId, @PathVariable Long productId,
                                            @RequestBody ProductDTO updatedProduct) {
+        Span span = tracer.buildSpan("Update product").start();
+        Tags.HTTP_METHOD.set(span, "PUT");
+        Tags.HTTP_URL.set(span, "/api/product/"+userId+"/"+productId);
+        span.finish();
         try{
             Product product = productService.updateProduct(userId, productId, updatedProduct);
             return new ResponseEntity<>(product, HttpStatus.OK);
