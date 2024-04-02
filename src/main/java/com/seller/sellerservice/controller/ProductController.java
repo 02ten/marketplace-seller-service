492db3e023fe41e24eb6ec2be20ec31ed613e78e
Ten Aleksandr Aleksandrovich <89169604848t@gmail.com>
2024-04-02 11:47:42 +0300

    Changed response status on DELETE

--- src/main/java/com/seller/sellerservice/controller/ProductController.java
+++ src/main/java/com/seller/sellerservice/controller/ProductController.java
@@ -44,7 +44,7 @@ public class ProductController {
             productService.deleteProduct(userId, productId);
             return new ResponseEntity<>("Товар успешно удален", HttpStatus.OK);
         } catch (IllegalArgumentException ex) {
-            return new ResponseEntity<>(ex.getMessage(), HttpStatus.OK);
+            return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }
 
