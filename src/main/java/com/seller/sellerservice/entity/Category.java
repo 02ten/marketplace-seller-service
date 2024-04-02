d747bf72d92ac60eea86905d65c6c5ca83f7db33
Ten Aleksandr Aleksandrovich <89169604848t@gmail.com>
2024-04-02 11:24:32 +0300

    Added @JsonIgnore to productList in category

--- src/main/java/com/seller/sellerservice/entity/Category.java
+++ src/main/java/com/seller/sellerservice/entity/Category.java
@@ -1,9 +1,11 @@
 package com.seller.sellerservice.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
@@ -12,11 +14,13 @@ import java.util.List;
 @Table(name = "t_category")
 @AllArgsConstructor
 @Getter
+@Setter
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
+    @JsonIgnore
     @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
     private List<Product> productList;
 }
